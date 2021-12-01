ef96bc0f1a15fc4edb494e37e933796ff37f0b0a
Remy Vaartjes <remy@xd.nl>
2021-12-01 11:25:42 +0100

    Remove true from Image() method
    Should not be there.

--- code/product/variations/ProductVariation.php
+++ code/product/variations/ProductVariation.php
@@ -346,7 +346,7 @@ class ProductVariation_OrderItem extends Product_OrderItem
         if (($this->ProductVariation()) && $this->ProductVariation()->Image()->exists()) {
             return $this->ProductVariation()->Image();
         }
-        return $this->Product(true)->Image();
+        return $this->Product()->Image();
     }
 
     public function Width() {
